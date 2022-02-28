5e0c64cd82032aee63a1c094311e7290b25ca02d
AlexChaPar <alexchapar@users.noreply.github.com>
2022-02-28 01:16:50 -0500

    Update the referenced pwsh profile file

--- windows-terminal/config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows-terminal/config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
+$env:POSH_GIT_ENABLED = $true
+
 Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/new-rebels.omp.json")
 
-$env:POSH_GIT_ENABLED = $true
 Import-Module posh-git
