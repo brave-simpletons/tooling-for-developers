b9fc9ea0b009560a6cc5976f7abb4bf83dd1c703
AlexChaPar <alexchapar@users.noreply.github.com>
2022-03-09 22:31:49 -0500

    Hiding sensitive data with the ability the showing them

--- windows-terminal/config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows-terminal/config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
 $env:POSH_GIT_ENABLED = $true
+# To use a specific char as a logo, use the character from its unicode ex :  => \uf1d0
+$env:OHMYPOSH_SHELL_LOGO = "" #  is the \uf1d0 (rebel) character
+# The "OHMYPOSH_SHOW_SENSITIVE_DATA" environment variable is used to enable "sensitive data" to be shown (useful to disabling it when presenting and still having a "kickass" shell)
+$env:OHMYPOSH_SHOW_SENSITIVE_DATA = $true # possible values to enabling sensitive data to be shown : [$true, "True", "true", 1] any other values will hide the sensitive data
 
 Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/new-rebels.omp.json")
 
